c5933403a5f987216801b2afccdae31700cf817c
Nikhil Singh <nikhil@aresourcepool.com>
2023-05-29 22:34:19 +0530

    react js and typescript

--- react-typescript-app/src/App.tsx
+++ react-typescript-app/src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoList } from "./components/TodoList";
 import { NewTodo } from "./components/NewTodo";
-
+import { Todo } from "./todo.model";
 const App: React.FC = () => {
-  const todos = [{ id: 1, name: "Course Finished" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const addTodoHandler = (text: string) => {
-    console.log(text, "---- name -");
+    setTodos((prevTodo) => [
+      ...prevTodo,
+      {
+        id: Math.random(),
+        name: text,
+      },
+    ]);
+  };
+  const todoDeleteHandler = (todoId: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
   };
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler} />
-      <TodoList todos={todos} />
+      <TodoList onDeleteTodo={todoDeleteHandler} todos={todos} />
     </div>
   );
 };
--- react-typescript-app/src/components/TodoList.tsx
+++ react-typescript-app/src/components/TodoList.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Todo } from "../todo.model";
 interface TodoListProps {
-  todos: { id: number; name: string }[];
+  todos: Todo[];
+  onDeleteTodo: (id: number) => void;
 }
-export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+export const TodoList: React.FC<TodoListProps> = ({ todos, onDeleteTodo }) => {
   return (
     <ul>
       {todos?.map((todo) => (
-        <li key={todo.id}>{todo?.name}</li>
+        <li key={todo.id}>
+          <span>
+            {todo?.name}
+            <button onClick={onDeleteTodo.bind(null, todo?.id)}>Delete</button>
+          </span>
+        </li>
       ))}
     </ul>
   );
